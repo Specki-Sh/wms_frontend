d9f53181431e75df94cb25c2b8173b0515aacd92
Shakh <yushakhzod@gmail.com>
2022-01-19 16:33:16 +0500

    init: Docker

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/api/";
+const baseURL = "http://localhost:8000/api/";
 const instance = axios.create({
   baseURL,
   timeout: 10000,
