fa8bbb1ae34ceae4e15b26d4cbd0d841f164379d
Specki-Sh <yushakhzod@gmail.com>
2022-12-15 09:33:17 +0500

    added types in functions

--- src/views/Acceptance.vue
+++ src/views/Acceptance.vue
@@ -22,8 +22,13 @@
 // vue
 import { defineComponent, computed } from "vue";
 // Models
-import { Acceptance as IAcceptance } from "@/api/models";
-import { Supplier as ISupplier, Product as IProduct } from "@/api/models";
+import {
+  Goods as IGoods,
+  AcceptanceDocument as IAcceptanceDocument,
+  Customer as ICustomer,
+  ProductCard as IProductCard,
+  DocumentInfo as IDocumentInfo
+} from "@/api/models";
 import { headers } from "@/api/models";
 // component
 import GoodsTableAction from "@/components/Goods/GoodsTableAction.vue";
@@ -42,19 +47,19 @@ export default defineComponent({
   data() {
     return {
       headers: headers.acceptance,
-      desserts: [] as Array<IAcceptance>,
+      desserts: [] as Array<IDocumentInfo>,
       totalPages: 0,
       page: 1,
       contractors: {
-        items: [] as Array<ISupplier>,
+        items: [] as Array<ICustomer>,
         totalPages: 0,
         headers: headers.supplier,
-      } as ITable<ISupplier>,
+      } as ITable<ICustomer>,
       products: {
-        items: [] as Array<IProduct>,
+        items: [] as Array<IProductCard>,
         totalPages: 0,
         headers: headers.product,
-      } as ITable<IProduct>,
+      } as ITable<IProductCard>,
     };
   },
   provide() {
@@ -64,12 +69,7 @@ export default defineComponent({
     };
   },
   methods: {
-    setDesserts(desserts: Array<IAcceptance>) {
-      desserts.forEach((dessert) => {
-        dessert["date"] = new Date(dessert["date"])
-          .toLocaleString()
-          .slice(0, 10);
-      });
+    setDesserts(desserts: Array<IDocumentInfo>) {
       this.desserts = desserts;
     },
     setTotalPages(total: number) {
@@ -82,7 +82,7 @@ export default defineComponent({
       this.setTotalPages(data.total);
     },
     addItem(item: any) {
-      let goods = [];
+      let goods: Array<IGoods> = [];
       for (const product of item.products){
         goods.push({
           product_id: product.id,
@@ -90,11 +90,11 @@ export default defineComponent({
           price: product.price,
         })
       }
-      const acceptance_document: IAcceptance = {
+      const acceptance_document: IAcceptanceDocument = {
         number: item.document_number,
         supplier_id: item.contractor.id,
         goods: goods,
-        date: item.date.toISOString().split('T')[0],
+        date: item.date,
       }
       api.acceptance.add(acceptance_document).then(
         () => {
@@ -104,7 +104,7 @@ export default defineComponent({
           console.warn("Function addItem in Acceptance.vue", error)
       );
     },
-    editItem(data: { id: number; item: IAcceptance }) {
+    editItem(data: { id: number; item: IAcceptanceDocument }) {
       api.acceptance.change(data.id, data.item).then(
         () => {
           this.updateData(this.page);
@@ -122,19 +122,19 @@ export default defineComponent({
           console.warn("Function removeItem in Acceptance.vue", error)
       );
     },
-    setContractorsItems(item: Array<ISupplier>) {
+    setContractorsItems(item: Array<ICustomer>) {
       this.contractors.items = item;
     },
     setContractorsTotalPages(total: number) {
       this.contractors.totalPages = (total - total % 10) / 10;
-        if (total % 10 != 0) {this.contractors.totalPages +=1}
+      if (total % 10 != 0) {this.contractors.totalPages +=1}
     },
     async updateContractors(page = 1) {
       const data = await api.supplier.getByPage(page);
       this.setContractorsItems(data.suppliers);
       this.setContractorsTotalPages(data.total);
     },
-    setProducts(item: Array<IProduct>) {
+    setProducts(item: Array<IProductCard>) {
       this.products.items = item;
     },
     setProductTotalPages(total: number) {
